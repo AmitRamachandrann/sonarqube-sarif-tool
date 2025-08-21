eff852f33ee08cd47ec41dd9f532f98dafa99753
AmitRamachandrann <aramachandran@cloudbees.com>
2025-08-21 08:40:26 +0530

    added helper

--- vars/helper.groovy
+++ vars/helper.groovy
@@ -1,21 +1,24 @@
-def convertSonarToSarif(sonarData) {
-    def sarifData = [
-        version: "2.1.0",
-        runs: [
-            [
-                tool: [
-                    driver: [
-                        name: "SonarQube",
-                        version: sonarData.version
-                    ]
-                ],
-                results: []
-            ]
-        ]
-    ]
+import groovy.json.JsonOutput
 
-    sonarData.issues.each { issue ->
-        sarifData.runs[0].results << [
+// Map SonarQube hotspots to issues format for SARIF conversion
+def mapHotspotsToIssues(hotspots) {
+    return hotspots.collect { hotspot ->
+        [
+            rule: hotspot.ruleKey,
+            message: hotspot.message,
+            filePath: hotspot.component.split(":")[1],
+            startLine: hotspot.textRange.startLine,
+            endLine: hotspot.textRange.endLine,
+            startColumn: hotspot.textRange.startOffset,
+            endColumn: hotspot.textRange.endOffset
+        ]
+    }
+}
+
+// Map issues directly to SARIF results format
+def mapIssuesToSarif(issues) {
+    return issues.collect { issue ->
+        [
             ruleId: issue.rule,
             message: issue.message,
             locations: [
@@ -35,6 +38,24 @@ def convertSonarToSarif(sonarData) {
             ]
         ]
     }
+}
 
+// Convert issues to SARIF JSON
+def convertIssuesToSarif(issues, sonarVersion = "9.9.0") {
+    def sarifData = [
+        version: "2.1.0",
+        runs: [
+            [
+                tool: [
+                    driver: [
+                        name: "SonarQube",
+                        version: sonarVersion
+                    ]
+                ],
+                results: mapIssuesToSarif(issues)
+            ]
+        ]
+    ]
+    println JsonOutput.prettyPrint(JsonOutput.toJson(sarifData))
     return sarifData
 }
